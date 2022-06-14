ab1040ba704ae820026abb08649f86132b763081
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-06-14 12:03:14 +0200

    Captain Bootstrap curse

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $templateParams["titolo"] = "Luminosità - Vicinanza . Silezio . Bevande";
 $templateParams["nome"] = "home.php";
 $templateParams["categorie"] = $dbh->getFoodTypes();
 $templateParams["user"] = "UTENZIONATOH";
+$templateParams["js"] = array("jquery-3.4.1.min.js", "header_clock.js");
 //Home Template
 
 require 'template/base.php';
