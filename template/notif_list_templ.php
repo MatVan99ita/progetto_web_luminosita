a73c9716137a92eab8446135ad455975456eadfa
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-07-27 16:27:36 +0200

    test for notification list

--- template/notif_list_templ.php
+++ template/notif_list_templ.php
@@ -1,15 +1,33 @@
-<ul class="list-group">
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-
-
-  <li class="list-group-item list-group-item-primary">A simple primary list group item</li>
-  <li class="list-group-item list-group-item-secondary">A simple secondary list group item</li>
-  <li class="list-group-item list-group-item-success">A simple success list group item</li>
-  <li class="list-group-item list-group-item-danger">A simple danger list group item</li>
-  <li class="list-group-item list-group-item-warning">A simple warning list group item</li>
-  <li class="list-group-item list-group-item-info">A simple info list group item</li>
-  <li class="list-group-item list-group-item-light">A simple light list group item</li>
-  <li class="list-group-item list-group-item-dark">A simple dark list group item</li>
+<ul id="notification_list" class="list-group">
+  <li class="list-group-item"><h2>N messaggi di cui M non letti</h2></li>
+<?php $class = array("primary", "light");
+$b = 0;
+for($i=0; $i<8; $i++): ?>
+  <li style="" class="list-group-item list-group-item-<?php echo $class[$b]; ?>">
+    <section class="section" aria-describedby="titleEx1">
+      <div class="section-content">
+        <!-- contenuto di esempio START -->
+        <div class="container">
+          <div class="row mb-3">
+            <div class="col-12">
+              <h2 id="titleEx1">Morbi fermentum amet</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6 col-xl-4 pe-0 pe-md-5 mb-3 font-serif">Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dictum sit amet justo donec enim diam vulputate ut. Eu nisl nunc mi ipsum faucibus.</div>
+            <div class="col-12 col-lg-6 col-xl-4 pe-0 pe-md-5 mb-3 font-serif">Eget egestas purus viverra accumsan. Diam maecenas ultricies mi eget mauris pharetra et. Etiam dignissim diam quis enim. Eu nisl nunc mi ipsum faucibus.</div>
+            <div class="col-12 col-lg-6 col-xl-4 pe-0 pe-md-5 font-serif"><a href="notifiche.php">Euismod lacinia at quis risus sed vulputate. Scelerisque purus semper eget duis at tellus at urna condimentum. Mattis enim ut tellus elementum sagittis.</a></div>
+          </div>
+        </div>
+        <!-- contenuto di esempio END -->
+      </div>
+    </section>
+  </li>
+<?php 
+if($i >= 4){
+  $b = 1;
+}
+endfor; ?>
 </ul>
 
 <div class="list-group">
