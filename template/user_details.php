4189c2fc53721b5458aa7c97e55b876c52784281
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-08-04 19:46:22 +0200

    removed oranges

--- template/user_details.php
+++ template/user_details.php
@@ -209,14 +209,3 @@ $pagah = $utente["info_pagamento"];
     </div>
     <?php endforeach; ?>
 </div>
-
-<div class="col">
-        <div class="card" style="width: 20rem;">
-            <img class="card-img-top" src="http://www.azspagirls.com/files/2010/09/orange.jpg" alt="Card image cap">
-            <div class="card-block">
-                <h4 class="card-title">Orange</h4>
-                <p class="card-text">Price: $0.5</p>
-                <a href="#" data-name="Orange" data-price="0.5" class="add-to-cart btn btn-primary">Add to cart</a>
-            </div>
-        </div>
-    </div>
